1e2e026f6517a5c5c5dd0287ddc0fff8911a8120
Bruno Santos <brcesar19@gmail.com>
2026-09-21 17:08:16 -0300

    chore: change consts declarations with const

--- src/core/consts/queue.consts.ts
+++ src/core/consts/queue.consts.ts
@@ -1,5 +1,5 @@
-export class QueueErrorMessagesConsts {
-  public static readonly INITIALIZING_QUEUE_ERROR = 'Error initialing queue, check credentials';
-  public static readonly SEND_TO_QUEUE_ERROR = 'Error sending message to queue';
-  public static readonly CONSUME_QUEUE_ERROR = 'Error trying to consume from queue';
-}
+export const QueueErrorMessagesConsts = {
+  INITIALIZING_QUEUE_ERROR: 'Error initialing queue, check credentials',
+  SEND_TO_QUEUE_ERROR: 'Error sending message to queue',
+  CONSUME_QUEUE_ERROR: 'Error trying to consume from queue',
+} as const;
--- src/core/consts/storage.consts.ts
+++ src/core/consts/storage.consts.ts
@@ -1,4 +1,4 @@
-export class StorageErrorMessagesConsts {
-  public static readonly INITIALIZING_STORAGE_ERROR = 'Error initialing storage, check credentials';
-  public static readonly SAVE_TO_STORAGE_ERROR = 'Error saving message to storage';
-}
+export const StorageErrorMessagesConsts = {
+  INITIALIZING_STORAGE_ERROR: 'Error initialing storage, check credentials',
+  SAVE_TO_STORAGE_ERROR: 'Error saving message to storage',
+} as const;
